65cf6184d3107077d86137ef2e8c20b15b06727c
brockbarlow <brock.barlow@students.aie.edu.au>
2016-08-25 17:25:21 -0500

    began working on translation generation assignment

---
+++ Assignments/Translation Generation/src/Main.cpp
@@ -0,0 +1,12 @@
+/*Write a program that generates a matrix translation for any identity matrix. 
+For example, what is the resulting matrix when transformation of [5,3,2,1] is applied 
+to the identity? This should work for any arbitrary matrix*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("pause");
+	return 0;
+}
