2e22c2f94d21a73ab1476c1c0db197557573e5f6
brockbarlow <brock.barlow@students.aie.edu.au>
2016-08-25 23:09:09 -0500

    commented out code

--- Assignments/Translation Generation/src/Main.cpp
+++ Assignments/Translation Generation/src/Main.cpp
@@ -1,12 +1,27 @@
 /*Write a program that generates a matrix translation for any identity matrix. 
-For example, what is the resulting matrix when transformation of [5,3,2,1] is applied 
+For example, what is the resulting matrix when the transformation of [5,3,2,1] is applied 
 to the identity? This should work for any arbitrary matrix*/
 
 #include <iostream>
 using namespace std;
 
+void translate()
+{
+	int identityMat[4][4];
+
+	/*for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			identityMat[i][j] = (i == j) ? 1 : 0;
+		}
+	}*/
+
+
+}
+
 int main()
 {
+	//int identityMat[4][4] = { {1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1} };
+
 	system("pause");
 	return 0;
 }
