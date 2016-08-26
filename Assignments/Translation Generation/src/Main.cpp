d6aca30275953adddb85ea2229d95e629c73b78e
brockbarlow <brock.barlow@students.aie.edu.au>
2016-08-26 17:30:32 -0500

    finished created identity function. working on translate function.

--- Assignments/Translation Generation/src/Main.cpp
+++ Assignments/Translation Generation/src/Main.cpp
@@ -5,22 +5,45 @@ to the identity? This should work for any arbitrary matrix*/
 #include <iostream>
 using namespace std;
 
-void translate()
+int** createIdentity(int rows, int cols)
 {
-	int identityMat[4][4];
+	int** identityMat;
+	identityMat = new int*[rows];
 
-	/*for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
+	for (int i = 0; i < rows; i++) {
+		identityMat[i] = new int[cols];
+		for (int j = 0; j < cols; j++) {
 			identityMat[i][j] = (i == j) ? 1 : 0;
+		}
+	}
+
+	//double offset = sqrt(rows * cols) - 1;
+
+	/*for (int i = 0; i < rows; i++) {
+		cout << "| ";
+		for (int j = 0; j < cols; j++) {
+			cout << identityMat[i][j] << " | ";
+			if (j == 3) {
+				cout << "\n";
+			}
 		}
 	}*/
 
+	return identityMat;
+}
+
+void performTranslate()
+{
 
 }
 
 int main()
 {
-	//int identityMat[4][4] = { {1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1} };
+	int rows = 4;
+	int cols = 4;
+
+	int** identityMat = createIdentity(rows, cols);
+	//performTranslate(identityMat);
 
 	system("pause");
 	return 0;
