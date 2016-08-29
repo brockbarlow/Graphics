6e85a47bfa010ba10c97fb7aff5c6abd5bdbb754
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-08-29 09:11:38 -0500

    created functions

--- Assignments/Generate Pro, Ortho, Per Matrices/src/Main.cpp
+++ Assignments/Generate Pro, Ortho, Per Matrices/src/Main.cpp
@@ -3,3 +3,30 @@ Try to do it without looking up the formula for orthographic.
 Think of the matrix and the box it makes, what type of parameters are required to make a cube 
 and what are there positions relative to the matrix? Hint: 2/right - left is x*/
 
+#include <iostream>
+
+int genProjection()
+{
+
+	return 0;
+}
+
+int genOrthographic()
+{
+
+	return 0;
+}
+
+int genPerspective()
+{
+
+	return 0;
+}
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
