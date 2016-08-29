b730bc8839aed8c3d6531c99a8ca0b3664aa99db
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-08-29 12:06:43 -0500

    finished orthographic function

--- Assignments/Generate Pro, Ortho, Per Matrices/src/Main.cpp
+++ Assignments/Generate Pro, Ortho, Per Matrices/src/Main.cpp
@@ -5,28 +5,73 @@ and what are there positions relative to the matrix? Hint: 2/right - left is x*/
 
 #include <iostream>
 
-int genOrthographic(int m, int r, int l, int t, int b, int n, int f)
+int genOrthographic(int r, int l, int t, int b, int n, int f)
 {
-	int matOrtho[4][4] = {0};
+	int matOrtho[4][4];
+	
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			matOrtho[i][j] = (i == j) ? 1 : 0;
+		}
+	}
+
+	matOrtho[0][0] = 2 / r - l;
+	matOrtho[0][3] = -(r + l / r - l);
+
+	matOrtho[1][1] = 2 / t - b;
+	matOrtho[1][3] = -(t + b / t - b);
+
+	matOrtho[2][2] = -2 / f - n;
+	matOrtho[2][3] = -(f + n / f - n);
 
 
 	return matOrtho;
 }
 
-int genPerspective(int m, int fov, int aspect, int n, int f)
+int genPerspective(int fov, int aspect, int n, int f)
 {
-	int matPersp;
+	int matPersp[4][4];
 
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			matPersp[i][j] = (i == j) ? 1 : 0;
+		}
+	}
+
+	//
 
 	return matPersp;
 }
 
 int main()
 {
-	int matA;
+	int newOrtho;
+	int newPersp;
 
-	//genOrthographic(matA);
-	//genPerspective(matA);
+	newOrtho = genOrthographic();
+	newPersp = genPerspective();
+
+	for (int i = 0; i < 4; i++) {
+		std::cout << "| ";
+		for (int j = 0; j < 4; j++) {
+			std::cout << newOrtho[i][j] << " | ";
+			if (j == 3) {
+				std::cout << "\n";
+			}
+		}
+	}
+
+	std::cout << std::endl;
+
+	for (int i = 0; i < 4; i++) {
+		std::cout << "| ";
+		for (int j = 0; j < 4; j++) {
+			std::cout << newPersp[i][j] << " | ";
+			if (j == 3) {
+				std::cout << "\n";
+			}
+		}
+	}
 
 	system("pause");
 	return 0;
