598d47557aee64689eeb4bbc7152ae8536446adf
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-08-29 08:55:42 -0500

    added generate pro, ortho and per matrices assignment

---
+++ Assignments/Generate Pro, Ortho, Per Matrices/src/Main.cpp
@@ -0,0 +1,5 @@
+/*Write functions to generate projection matrices, orthographic and perspective view matrices. 
+Try to do it without looking up the formula for orthographic.  
+Think of the matrix and the box it makes, what type of parameters are required to make a cube 
+and what are there positions relative to the matrix? Hint: 2/right - left is x*/
+
