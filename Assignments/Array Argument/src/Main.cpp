c5e56d2502e04b3b64bbf85ed6347c1560ad8fb5
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-08-30 11:29:41 -0500

    added block statement

--- Assignments/Array Argument/src/Main.cpp
+++ Assignments/Array Argument/src/Main.cpp
@@ -5,19 +5,20 @@ This is gist 5*/
 
 #include <iostream>
 
-void argumentOne()
+void ArgumentOne()
 {
 	std::cout << "argument function one" << std::endl;
 	std::cout << std::endl;
 }
 
-void argumentTwo()
+void ArgumentTwo()
 {
 	int num1, num2, answer;
 
 	answer = num1 + num2;
 
 	std::cout << answer << std::endl;
+	std::cout << std::endl;
 }
 
 int main()
@@ -26,3 +27,8 @@ int main()
 	system("pause");
 	return 0;
 }
+
+/*could not finish assignment. had code block after making 
+functions. block began when I needed to "takes another 
+function as an argument. tried looking on google for help.
+became more confused.*/
