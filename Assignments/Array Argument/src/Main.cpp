c2f316b7f7b1b86ab2955a87dde93c5056baa099
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-08-30 11:12:09 -0500

    created two functions

--- Assignments/Array Argument/src/Main.cpp
+++ Assignments/Array Argument/src/Main.cpp
@@ -5,8 +5,24 @@ This is gist 5*/
 
 #include <iostream>
 
+void argumentOne()
+{
+	std::cout << "argument function one" << std::endl;
+	std::cout << std::endl;
+}
+
+void argumentTwo()
+{
+	int num1, num2, answer;
+
+	answer = num1 + num2;
+
+	std::cout << answer << std::endl;
+}
+
 int main()
 {
+
 	system("pause");
 	return 0;
 }
