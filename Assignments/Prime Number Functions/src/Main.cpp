eb443442b05b6f8df1054f9981cc30fd7747b6b3
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-08-31 08:50:42 -0500

    calculated prime

--- Assignments/Prime Number Functions/src/Main.cpp
+++ Assignments/Prime Number Functions/src/Main.cpp
@@ -13,12 +13,39 @@ below 1000.
 
 void calculatePrime()
 {
-
+	/*for (int i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			std::cout << "number is not prime" << std::endl;
+			break;
+		}
+		else if (num % i != 0)
+		{
+			std::cout << "number is prime" << std::endl;
+			break;
+		}
+	}*/
 }
 
 void findUnder100()
 {
-
+	for (int i = 2; i < 100; i++)
+	{
+		bool prime = true;
+		for (int j = 2; j * j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				prime = false;
+				break;
+			}
+		}
+		if (prime)
+		{
+			std::cout << i << " " << std::endl;
+		}
+	}
 }
 
 void determineMultiple()
@@ -28,12 +55,30 @@ void determineMultiple()
 
 void findUnder1000()
 {
-
+	for (int i = 2; i < 1000; i++)
+	{
+		bool prime = true;
+		for (int j = 2; j * j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				prime = false;
+				break;
+			}
+		}
+		if (prime)
+		{
+			std::cout << i << " " << std::endl;
+		}
+	}
 }
 
 int main()
 {
-
+	//int num;
+	//std::cout << "Enter number:" << std::endl;
+	//std::cin >> num;
+	calculatePrime();
 
 	system("pause");
 	return 0;
@@ -41,3 +86,5 @@ int main()
 
 /*A prime number is a number greater than one that has
 no positive divisors other than one and itself.*/
+
+/*Was able to complete Part 1 of assignment.*/
