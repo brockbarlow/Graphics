5da91131a726d2807502e61ee0149aec47424092
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-08-31 09:29:32 -0500

    redid code

--- Assignments/Prime Number Functions/src/Main.cpp
+++ Assignments/Prime Number Functions/src/Main.cpp
@@ -11,80 +11,122 @@ below 1000.
 
 #include <iostream>
 
-void calculatePrime()
+void isPrime(int num)
 {
-	/*for (int i = 2; i < num; i++)
+	for (int i = 2; i < num; i++)
 	{
 		if (num % i == 0)
 		{
-			std::cout << "number is not prime" << std::endl;
+			//number is not prime
 			break;
 		}
 		else if (num % i != 0)
 		{
-			std::cout << "number is prime" << std::endl;
+			//number is prime
 			break;
 		}
-	}*/
-}
-
-void findUnder100()
-{
-	for (int i = 2; i < 100; i++)
-	{
-		bool prime = true;
-		for (int j = 2; j * j <= i; j++)
-		{
-			if (i % j == 0)
-			{
-				prime = false;
-				break;
-			}
-		}
-		if (prime)
-		{
-			std::cout << i << " " << std::endl;
-		}
 	}
 }
 
-void determineMultiple()
+void isMultiple()
 {
 
 }
 
-void findUnder1000()
+void calculate()
 {
-	for (int i = 2; i < 1000; i++)
-	{
-		bool prime = true;
-		for (int j = 2; j * j <= i; j++)
-		{
-			if (i % j == 0)
-			{
-				prime = false;
-				break;
-			}
-		}
-		if (prime)
-		{
-			std::cout << i << " " << std::endl;
-		}
-	}
+
 }
 
 int main()
 {
-	//int num;
-	//std::cout << "Enter number:" << std::endl;
-	//std::cin >> num;
-	calculatePrime();
+	int under100 = 100;
+	int under1000 = 1000;
 
 	system("pause");
 	return 0;
+
+	//int num;
+	//std::cout << "Enter number:" << std::endl;
+	//std::cin >> num;
+	//calculatePrime();
+	//findUnder100();
 }
 
 /*A prime number is a number greater than one that has
 no positive divisors other than one and itself.*/
 
 /*Was able to complete Part 1 of assignment.*/
+/*Part 2 of assignment giving me trouble. Trying
+to call function from another function.*/
+
+/*Should have only three functions: isprime(), isMultiple()
+and calculate()*/
+
+/*Old code:
+
+//void calculatePrime()
+//{
+//
+//
+//	//void findUnder100();
+//
+//	/*for (int i = 2; i < num; i++)
+//	{
+//		if (num % i == 0)
+//		{
+//			std::cout << "number is not prime" << std::endl;
+//			break;
+//		}
+//		else if (num % i != 0)
+//		{
+//			std::cout << "number is prime" << std::endl;
+//			break;
+//		}
+//	}
+//}
+//
+//void findUnder100()
+//{
+//	for (int i = 2; i < 100; i++)
+//	{
+//		bool prime = true;
+//		for (int j = 2; j * j <= i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				prime = false;
+//				break;
+//			}
+//		}
+//		if (prime)
+//		{
+//			std::cout << i << " " << std::endl;
+//		}
+//	}
+//}
+//
+//void determineMultiple()
+//{
+//
+//}
+//
+//void findUnder1000()
+//{
+//	for (int i = 2; i < 1000; i++)
+//	{
+//		bool prime = true;
+//		for (int j = 2; j * j <= i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				prime = false;
+//				break;
+//			}
+//		}
+//		if (prime)
+//		{
+//			std::cout << i << " " << std::endl;
+//		}
+//	}
+//}*/
