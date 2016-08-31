1884d50be3975e946bf9a8ecea9d2185cc5889a9
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-08-31 08:21:26 -0500

    added gist 6 assignment

---
+++ Assignments/Prime Number Functions/src/Main.cpp
@@ -0,0 +1,23 @@
+/*Create a function to calculate a prime number 
+(Show on paper or in comments what you think a prime 
+number is. Do not google it...). 
+Write a function that uses that function to find all 
+prime numbers under 100.
+Write a function that determines if a number is a 
+multiple of another number. 
+Use your functions together to find all prime numbers 
+below 1000.
+*/
+
+#include <iostream>
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
+
+/*A prime number is a number greater than one that has
+no positive divisors other than one and itself.*/
