0d7a53d40c04139a8d1878b028f30d61902cde5a
brockbarlow <brock.barlow@students.aie.edu.au>
2016-09-01 23:25:52 -0500

    finished rework

--- Assignments/Identity Matrix Creation/src/Main.cpp
+++ Assignments/Identity Matrix Creation/src/Main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
-void createIdentity(int, int);
+void createIdentity(int, int); //prototype
 
-int rows, cols;
+int rows, cols; //size of matrix. user defined
 
 int main()
 {
 	std::cout << "Enter rows and columns" << "\n" << std::endl;
 	
 	std::cout << "Rows: ";
-	std::cin >> rows;
+	std::cin >> rows; //user input data
 
 	std::cout << "Columns: ";
-	std::cin >> cols;
+	std::cin >> cols; //user input data
 	std::cout << "\n" << std::endl;
 	 
 	createIdentity(rows, cols);
@@ -27,20 +27,34 @@ void createIdentity(int r, int c)
 	int** identityMat = 0;
 
 	identityMat = new int*[r];
-
 	for (int i = 0; i < r; i++)
 	{
-		identityMat[i] = new int[c];
+		identityMat[i] = new int[c];      //create matrix
 		for (int j = 0; j < c; j++)
 		{
 			identityMat[i][j] = (i == j);
 		}
 	}
 
-	for (int i = 0; i < r; i++) {
+	for (int i = 0; i < r; i++) {   //print matrix
 		std::cout << "| ";
 		for (int j = 0; j < c; j++) {
 			std::cout << identityMat[i][j] << " | ";
 		}
 	}
 }
+
+/*Test Case One:
+
+User inputs 3, 3 (3 rows and 3 columns).
+
+Test Result:
+identityMat = {{1,0,0}, {0,1,0}, {0,0,1}}
+
+Test Case Two:
+
+User inputs 4, 4 (4 rows and 4 columns).
+
+Test Result:
+identityMat = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}
+*/
