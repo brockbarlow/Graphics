7eba03cfc298dfaaa1882e23ba2f35ae09993343
brockbarlow <brock.barlow@students.aie.edu.au>
2016-09-01 22:31:14 -0500

    reworked gist 2

--- Assignments/Identity Matrix Creation/src/Main.cpp
+++ Assignments/Identity Matrix Creation/src/Main.cpp
@@ -6,14 +6,15 @@ int main()
 {
 	int rows, cols;
 
-	std::cout << "Enter rows and columns" << std::endl;
+	std::cout << "Enter rows and columns" << "\n" << std::endl;
 	
-	std::cout << "Rows:" << std::endl;
+	std::cout << "Rows: ";
 	std::cin >> rows;
 
-	std::cout << "Columns:" << std::endl;
+	std::cout << "Columns: ";
 	std::cin >> cols;
-
+	std::cout << "\n" << std::endl;
+	 
 	createIdentity(rows, cols);
 	std::cout << "\n" << std::endl;
 
@@ -23,21 +24,23 @@ int main()
 
 void createIdentity(int r, int c)
 {
-	int identityMat[4][4] = { 0 };
+	int** identityMat = 0;
 
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
+	identityMat = new int*[r];
+
+	for (int i = 0; i < r; i++)
+	{
+		identityMat[i] = new int[c];
+		for (int j = 0; j < c; j++)
+		{
 			identityMat[i][j] = (i == j);
 		}
 	}
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < r; i++) {
 		std::cout << "| ";
-		for (int j = 0; j < 4; j++) {
+		for (int j = 0; j < c; j++) {
 			std::cout << identityMat[i][j] << " | ";
-			if (j == 3) {
-				std::cout << "\n";
-			}
 		}
 	}
 }
