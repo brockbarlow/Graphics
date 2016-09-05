5181e6f11e7aa4cc0957eed3d3e7cbb725e65cdb
brockbarlow <brock.barlow@students.aie.edu.au>
2016-09-05 18:31:40 -0500

    made a small change

--- Assignments/Translation Generation/src/Main.cpp
+++ Assignments/Translation Generation/src/Main.cpp
@@ -11,19 +11,18 @@ int** createIdentity(int rows, int cols)
 			identityMat[i][j] = (i == j);
 		}
 	}
-
 	return identityMat;
 }
 
 void performTranslate(int** identityMat)
 {
-	int matA[4] = {5,3,2,1};
+	int vecA[4] = {5,3,2,1};
 	int translatedMat[4][4];
 
-	translatedMat[0][0] = identityMat[0][0] * matA[0] + identityMat[0][1] * matA[1] + identityMat[0][2] * matA[2] + identityMat[0][3] * matA[3];
-	translatedMat[1][0] = identityMat[1][0] * matA[0] + identityMat[1][1] * matA[1] + identityMat[1][2] * matA[2] + identityMat[1][3] * matA[3];
-	translatedMat[2][0] = identityMat[2][0] * matA[0] + identityMat[2][1] * matA[1] + identityMat[2][2] * matA[2] + identityMat[2][3] * matA[3];
-	translatedMat[3][0] = identityMat[3][0] * matA[0] + identityMat[3][1] * matA[1] + identityMat[3][2] * matA[2] + identityMat[3][3] * matA[3];
+	translatedMat[0][0] = identityMat[0][0] * vecA[0] + identityMat[0][1] * vecA[1] + identityMat[0][2] * vecA[2] + identityMat[0][3] * vecA[3];
+	translatedMat[1][0] = identityMat[1][0] * vecA[0] + identityMat[1][1] * vecA[1] + identityMat[1][2] * vecA[2] + identityMat[1][3] * vecA[3];
+	translatedMat[2][0] = identityMat[2][0] * vecA[0] + identityMat[2][1] * vecA[1] + identityMat[2][2] * vecA[2] + identityMat[2][3] * vecA[3];
+	translatedMat[3][0] = identityMat[3][0] * vecA[0] + identityMat[3][1] * vecA[1] + identityMat[3][2] * vecA[2] + identityMat[3][3] * vecA[3];
 
 	for (int i = 0; i < 4; i++) {
 		std::cout << *translatedMat[i] << " ";
@@ -34,7 +33,6 @@ int main()
 {
 	int rows = 4;
 	int cols = 4;
-
 	int** identityMat = createIdentity(rows, cols);
 	performTranslate(identityMat);
 
