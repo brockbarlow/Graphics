3805a64577cf90d79bb97cd1f204473e5e665f4b
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-06 09:12:29 -0500

    added assignment question

--- Assignments/Min and Max Functions/src/Main.cpp
+++ Assignments/Min and Max Functions/src/Main.cpp
@@ -1,4 +1,13 @@
+/*Write a function that takes a function as a parameter. 
+The function it receives should take two arguments (min,max). 
+Calculate a random number between those two numbers and 
+return it. If the function fails return or assert on the 
+call. If the function that takes the min,max function 
+fails give feedback to the user.*/
+
 #include <iostream>
+
+
 
 int main()
 {
