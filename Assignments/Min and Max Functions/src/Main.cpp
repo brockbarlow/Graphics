ae34b21a6db2683eb39bbcc860593dd7f8e78498
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-06 10:45:53 -0500

    edited comments

--- Assignments/Min and Max Functions/src/Main.cpp
+++ Assignments/Min and Max Functions/src/Main.cpp
@@ -50,13 +50,18 @@ int main()
 
 /*
 Things that I understood about assignment:
-1) create functions.
-2) have functions take arguments.
+1) Create functions.
+2) Have functions take arguments.
+3) Understand the "function takes a function parameter"
+   concept.
+4) Understood that the calculate function needs to return
+   something.
 
 Problems with assignment:
 1) Had trouble passing a function to another function as
    a parameter.
-2) Never used assert before (refer to test case in function).
+2) Never used assert before (refer to test case in 
+   calculate function).
 3) Didn't understand how calculation needed to be performed
    for calculate function.
 4) Didn't understand how to perform fail check for the
