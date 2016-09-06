f29ee3cbfd51b170ea2bc4f9ffea3dbb2d54f7b2
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-06 17:56:30 -0500

    defined start function

--- Tutorials/Rendering Geometry/src/Render.cpp
+++ Tutorials/Rendering Geometry/src/Render.cpp
@@ -58,7 +58,47 @@ void RenderingGeometry::generateGrid(unsigned int rows, unsigned int cols)
 
 bool RenderingGeometry::start()
 {
+	const char* vsSource = "#version 410\n \
+                            layout(location=0) in vec4 position; \
+                            layout(location=1) in vec4 color; \
+                            out vec4 vColor; \
+                            uniform mat4 projectionViewWorldMatrix; \
+                            void main() {vColor = color; gl_Position = projectionViewWorldMatrix * position; }";
 
+	const char* fsSource = "#version 410\n \
+                            in vec4 vColor; \
+                            out vec4 fragColor; \
+                            void main() {fragColor = vColor; }";
+
+	int success = GL_FALSE;
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vertexShader, 1, (const char**)&vsSource, 0);
+	glCompileShader(vertexShader);
+	glShaderSource(fragmentShader, 1, (const char**)&fsSource, 0);
+	glCompileShader(fragmentShader);
+
+	m_programID = glCreateProgram();
+	glAttachShader(m_programID, vertexShader);
+	glAttachShader(m_programID, fragmentShader);
+	glLinkProgram(m_programID);
+
+	glGetProgramiv(m_programID, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE)
+	{
+		int infoLogLength = 0;
+		glGetProgramiv(m_programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+		char* infoLog = new char[infoLogLength];
+
+		glGetProgramInfoLog(m_programID, infoLogLength, 0, infoLog);
+		printf("Error: Failed to link shader program!\n");
+		printf("%s\n", infoLog);
+		delete[] infoLog;
+	}
+
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
 }
 
 bool RenderingGeometry::update()
