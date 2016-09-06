d9a861f6f75f5c78adbb8a01d46c8aa8c732ea50
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-06 10:08:50 -0500

    added assert header to assignment

--- Assignments/Min and Max Functions/src/Main.cpp
+++ Assignments/Min and Max Functions/src/Main.cpp
@@ -6,14 +6,11 @@ call. If the function that takes the min,max function
 fails give feedback to the user.*/
 
 #include <iostream>
+#include <assert.h>
 
 int calculate(int max, int min)
 {
-	int ran;
-	std::cout << "enter random.";
-	std::cin >> ran;
-
-
+	assert(min >= max && "Error! Min is greater!");
 }
 
 void takeParameter(int(*function)())
@@ -23,11 +20,11 @@ void takeParameter(int(*function)())
 
 int main()
 {
-	int max, min;
+	int max, min = 0;
 
-	std::cout << "enter max.";
+	std::cout << "Enter max." << std::endl;
 	std::cin >> max;
-	std::cout << "enter min.";
+	std::cout << "Enter min." << std::endl;
 	std::cin >> min;
 
 	calculate(max, min);
