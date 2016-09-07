592fa844e95f942679d8a6000bbced94e75165fb
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-07 08:26:49 -0500

    created gist 8 assignment

---
+++ Assignments/Character Array Length/src/Main.cpp
@@ -0,0 +1,23 @@
+/*Write a function that calculates the length of a 
+character array using only pointers. Pass that function 
+as an argument to another function to evaluate it's 
+correctness.
+
+ex: char c[] = {"hello"}
+
+ex 2: typedef (void*)Callback(char c[]);
+      char c[] = "hello";
+	  Invoke ((Callback)doit, c);
+*/
+
+#include <iostream>
+
+
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
