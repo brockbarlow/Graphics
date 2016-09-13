38ab8dfea112fb49378547a6d2a446340642590f
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-13 09:03:38 -0500

    added assignment as comment

--- Assignments/Draw Circle/src/Main.cpp
+++ Assignments/Draw Circle/src/Main.cpp
@@ -1,7 +1,21 @@
+/*
+Write a function that takes arguments to create a 
+half circle. Parameters should be a radius number of 
+points and the return value should be the half circle.
+
+If you want to pass by ref that is fine too.
+
+This function should be tested and done on paper first. 
+Prove your answer for a set amount of points.
+*/
+
 #include <iostream>
+
+
 
 int main()
 {
+
 	system("pause");
 	return 0;
 }
