17237f702b384ea55e8e517ec6b14a8f39908cec
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-13 10:24:23 -0500

    created half circle function

--- Assignments/Draw Circle/src/Main.cpp
+++ Assignments/Draw Circle/src/Main.cpp
@@ -10,12 +10,40 @@ Prove your answer for a set amount of points.
 */
 
 #include <iostream>
+#include <cmath>
 
+struct vec3
+{
+	float x;
+	float y;
+	float z;
+};
 
+float halfCircle(float np, float rad)
+{
+	float pi = 3.14159265359f;
+	vec3 vertex[5];
+	float angle;
+
+	for (int i = 0; i < np - 1; i++)
+	{
+		angle = pi * (i / (np - 1));
+		vertex[i].x = rad + cos(angle);
+		vertex[i].y = rad + sin(angle);
+		vertex[i].z = 0;
+	}
+	return 0;
+}
 
 int main()
 {
+	float points = 5;
+	float rads = 1;
+
+	halfCircle(points, rads);
 
 	system("pause");
 	return 0;
 }
+
+/*I need help.*/
