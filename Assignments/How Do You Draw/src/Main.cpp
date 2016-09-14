e7f22e0f412b17408b98c9f8f67d49f7598eb775
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-14 08:43:30 -0500

    working on assignment

--- Assignments/How Do You Draw/src/Main.cpp
+++ Assignments/How Do You Draw/src/Main.cpp
@@ -7,3 +7,14 @@ code as reference and notes. This is an assessable
 question after 4 weeks of graphics.
 */
 
+/*
+"How do you draw?"
+
+Before we begin drawing, data buffers need to be created that
+OpenGL reads from (vertex and index information). A vertex
+struct should be created to hold vertex information. This
+struct will hold the position and color of data type vec4.
+We need three varibles of data type unsigned int. Three
+varibles are m_VAO, m_VBO, and m_IBO. 
+
+*/
