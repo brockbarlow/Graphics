6e9720179ffd559b2be363ab34e55f0b09a46280
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-14 07:58:52 -0500

    added question to gist

---
+++ Assignments/How Do You Draw/src/Main.cpp
@@ -0,0 +1,9 @@
+/*
+Morning gist:
+
+In your own words, answer this very simple question.
+"How do you draw?" You have 2 hrs and you can use your own
+code as reference and notes. This is an assessable 
+question after 4 weeks of graphics.
+*/
+
