60079a429f8d8c039f0a150b5f478eb46ef8c1f7
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-19 09:55:18 -0500

    finised assignment

--- Assignments/Gist 12/src/Main.cpp
+++ Assignments/Gist 12/src/Main.cpp
@@ -8,7 +8,15 @@ integers. Ex: Uint mult(uint x1, uint x2){}*/
 
 unsigned int multiply(unsigned int num1, unsigned int num2)
 {
+	unsigned int value = 0;
 
+	while (num2 != 0)
+	{
+		value = value + num1;
+		num2--;
+	}
+
+	return value;
 }
 
 int main()
@@ -23,7 +31,6 @@ int main()
 	returnedValue = multiply(num1, num2);
 	std::cout << returnedValue << std::endl;
 
-
 	system("pause");
 	return 0;
 }
@@ -33,11 +40,24 @@ Problem 1:
 How is it possible to check if a number is positive,
 negative or zero without using >, >=, <, <=?
 
-Possible Solution to Problem 1:
+Possible solution to Problem 1:
 Instead of using >, >=, <, and <=, we could use:
-!>, !>=, !<, !<=.
+!>, !>=, !<, !<=, ==, = and !=.
 
 Problem 2:
 Confused about question. We need a multiply function, but
 we can only addition and subtraction?
+
+Possible solution to Problem 2:
+Use a while loop to check if the second number doesn't
+equal zero. Take the value variable and number one variable
+and add them. Then decrement the second number.
+
+Side Note:
+Unsigned ints are always positive. If unsigned ints
+become negative, program will throw a compile error or
+will not fully function, so we don't need to check for
+negative values. Since uints are always positive, we 
+don't need to check if the values are positive. So for 
+this gist, we just need to check for zero.
 */
