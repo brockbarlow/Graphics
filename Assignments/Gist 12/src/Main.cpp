bd2503931ed0f2552f1d13485cde2d4853435a96
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-19 09:00:48 -0500

    created gist 12 assignment

---
+++ Assignments/Gist 12/src/Main.cpp
@@ -0,0 +1,16 @@
+/*Gist 12: write a function that multiplies two numbers. 
+You can only use addition or subtraction and you can only 
+check whether a number is positive negative or 0. Can not 
+use > >= < <=
+
+Return and parameters are unsigned integers
+
+Uint mult(uint x1, uint x2){}*/
+
+#include <iostream>
+
+int main()
+{
+	system("pause");
+	return 0;
+}
