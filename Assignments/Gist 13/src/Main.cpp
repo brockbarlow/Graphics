0b8db0d44946bba1fb3fc78ea08cc072880c0b30
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-21 07:51:47 -0500

    created gist 13

---
+++ Assignments/Gist 13/src/Main.cpp
@@ -0,0 +1,18 @@
+/*
+Gist 13: answer the following questions and submit via gist by 930.
+
+1. What is the difference between a directional and point light
+2. Which lighting model have you implemented?
+
+3. What are the equations for your model?
+
+4. Explain in detail how a diffuse calculation is made.
+
+5. Explain in detail how information goes from an application to
+the shader pipeline.
+
+You can not use the Internet or code. You can use typed notes or 
+handwritten notes. If you do not know the answer then put a valid 
+reason for not understanding it and how someone could help you
+*/
+
