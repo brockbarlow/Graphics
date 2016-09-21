dfb176cfb59f2d033c5726f416a228ba46e44cbc
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-21 12:47:01 -0500

    added new variables

--- Tutorials/Rendering Geometry/src/Render.h
+++ Tutorials/Rendering Geometry/src/Render.h
@@ -16,12 +16,15 @@ public:
 	void destroy() override;
 	std::string ReadFromFile(std::string text);
 	void createPlane();
+	void drawPlane();
 	void createCube();
+	void drawCube();
 	Vertex* generateHalfSphereVertices(unsigned int np, const int &rad);
 	Vertex* generateSphereVertices(const unsigned int &sides, const unsigned int &mirid, Vertex* &halfSphere);
 	unsigned int* generateSphereIndicies(const unsigned int &vertices, const unsigned int &mirid);
 	std::vector<unsigned int> indicesHolder;
 	void createSphere();
+	void drawSphere();
 private:
 	glm::mat4 m_projectionViewMatrix;
 	glm::mat4 view;
@@ -31,6 +34,22 @@ private:
 	unsigned int m_VAO;
 	unsigned int m_VBO;
 	unsigned int m_IBO;
+	//
+	unsigned int p_VAO;
+	unsigned int p_VBO;
+	unsigned int p_IBO;
+	unsigned int c_VAO;
+	unsigned int c_VBO;
+	unsigned int c_IBO;
+	unsigned int s_VAO;
+	unsigned int s_VBO;
+	unsigned int s_IBO;
+	//
 	int indicesCounter;
+	//
+	int p_indicesCounter;
+	int c_indicesCounter;
+	int s_indicesCounter;
+	//
 	float pi = 3.14159265359;
 };
