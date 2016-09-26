5f3a14ebb6a69b634aff09a7c3c60c8dfc2a5e08
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-26 10:03:47 -0500

    made edits to gist 14

--- Assignments/Gist 14/src/Main.cpp
+++ Assignments/Gist 14/src/Main.cpp
@@ -2,6 +2,7 @@
 Question 1: Distance from a Rectangle
 
 We expect this question to take approximately 20-40 minutes.
+
 Write a function that tests if a point falls within a 
 specified distance (“dist”) of any part of a solid, 2D rectangle. 
 The rectangle is specified by the bottom left corner, a width, 
@@ -42,17 +43,43 @@ struct Point
 struct Rectangle
 {
 	Point bottomLeft;
-	float width;
-	float height;
+	float width; 
+	float height; 
 };
 
 bool IsPointWithinDistOfRectangle(const Rectangle& rect, const Point& point, float dist)
 {
-	// TODO: implement
+	if (point.x <= (rect.bottomLeft.x + rect.width + dist) && point.x >= (rect.bottomLeft.x - dist))
+	{
+		if (point.y <= (rect.bottomLeft.y + rect.height + dist) && point.y >= (rect.bottomLeft.y - dist))
+		{
+			std::cout << "Point within dist of rectangle." << std::endl;
+			return true;
+		}
+	}
+	std::cout << "Error! Check failed." << std::endl;
+	return false;
 }
 
 int main()
 {
+	Point p;
+	p.x = 0.0f;
+	p.y = 0.0f;
+
+	Point p_test;
+	p_test.x = 10.0f;
+	p_test.y = 10.0f;
+
+	Rectangle r;
+	r.bottomLeft = p;
+	r.height = 5.0f;
+	r.width = 5.0f;
+
+	float dist = 5.0f;
+
+	IsPointWithinDistOfRectangle(p, p_test, dist);
+
 	system("pause");
 	return 0;
 }
