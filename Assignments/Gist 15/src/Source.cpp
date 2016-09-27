8b1a93aa7737bdeeec6e20a10e4a553a92d958d6
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-27 09:59:58 -0500

    working on gist 15

--- Assignments/Gist 15/src/Source.cpp
+++ Assignments/Gist 15/src/Source.cpp
@@ -22,35 +22,61 @@ float GetDps(Weapon w)
 class Weapon
 {
 public:
-	Weapon()
-	{
-
-	}
+	Weapon() {}
+	float minDamage;
+	float maxDamage;
+	float speed;
 };
 
-class Handgun : public Weapon
-{
-
-};
-
-class Shotgun : public Weapon
-{
-
-};
-
-class Rifle : public Weapon
-{
-
-};
+class HandGun : public Weapon {};
+class ShotGun : public Weapon {};
+class SniperRifle : public Weapon {};
+class FatMan : public Weapon {};
+class SubMachineGun : public Weapon {};
 
 float GetDPS(Weapon w)
-{
-
+{           
+	return ((w.minDamage + w.maxDamage) / 2) / w.speed;
 }
 
 int main()
 {
+	HandGun h;
+	h.minDamage = 12.0f;
+	h.maxDamage = 18.0f;
+	h.speed = 3.25f;
 
+	ShotGun s;
+	s.minDamage = 22.0f;
+	s.maxDamage = 36.0f;
+	s.speed = 2.09f;
+
+	SniperRifle sr;
+	sr.minDamage = 52.0f;
+	sr.maxDamage = 75.0f;
+	sr.speed = 1.18f;
+
+	FatMan f;
+	f.minDamage = 175.0f;
+	f.maxDamage = 268.0f;
+	f.speed = 0.38f;
+
+	SubMachineGun smg;
+	smg.minDamage = 11.0f;
+	smg.maxDamage = 15.0f;
+	smg.speed = 5.85f;
+
+	float temp_H = GetDPS(h);
+	float temp_S = GetDPS(s);
+	float temp_SR = GetDPS(sr);
+	float temp_F = GetDPS(f);
+	float temp_SMG = GetDPS(smg);
+
+	std::cout << "Handgun DPS is: " << temp_H << std::endl;
+	std::cout << "Shotgun DPS is: " << temp_S << std::endl;
+	std::cout << "Sniper Rigle DPS is: " << temp_SR << std::endl;
+	std::cout << "Fatman DPS is: " << temp_F << std::endl;
+	std::cout << "Sub Machinegun DPS is: " << temp_SMG << std::endl;
 
 	system("pause");
 	return 0;
