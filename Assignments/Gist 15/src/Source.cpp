5637c2c9564d5534591015cd58c8116ea6f7682d
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-09-27 08:55:37 -0500

    created gist 15

---
+++ Assignments/Gist 15/src/Source.cpp
@@ -0,0 +1,27 @@
+/*
+Question: Weapon DPS
+
+We expect this question to take approximately 30-45 minutes.
+Write a function that calculates the damage per second of a given weapon.
+
+Requirements: Function must take a parameter of type that inherits from Weapon.
+Return value must be to the nearest second decimal place. Ex: 35.6
+
+class Weapon
+{
+}
+
+float GetDps(Weapon w)
+{
+}
+*/
+
+#include <iostream>
+
+
+
+int main()
+{
+	system("pause");
+	return 0;
+}
